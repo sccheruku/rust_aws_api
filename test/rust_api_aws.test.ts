6b6a19cf4c32ac26e26cf625858d74b5e5db5c73
Sai C <saichaitanya88@gmail.com>
2022-05-17 22:36:31 -0400

    Initial commit

---
+++ test/rust_api_aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RustApiAws from '../lib/rust_api_aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rust_api_aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RustApiAws.RustApiAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
